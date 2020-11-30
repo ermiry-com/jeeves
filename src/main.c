67abe8db1fcf80410f7bdf7a8f06de75573405dc
ermiry <erick_salas@ermiry.com>
2020-11-30 02:36:14 -0600

    Updated jeeves service http cerver configuration

--- src/main.c
+++ src/main.c
@@ -17,6 +17,10 @@
 #include "handler.h"
 #include "version.h"
 
+#include "controllers/users.h"
+
+#include "routes/users.h"
+
 Cerver *jeeves_cerver = NULL;
 
 static void end (int dummy) {
@@ -49,10 +53,29 @@ static void jeeves_set_routes (HttpCerver *http_cerver) {
 	HttpRoute *jeeves_version_route = http_route_create (REQUEST_METHOD_GET, "version", jeeves_version_handler);
 	http_route_child_add (jeeves_route, jeeves_version_route);
 
-	// TODO:
 	// GET /jeeves/auth
 	HttpRoute *jeeves_auth_route = http_route_create (REQUEST_METHOD_GET, "auth", jeeves_auth_handler);
+	http_route_set_auth (jeeves_auth_route, HTTP_ROUTE_AUTH_TYPE_BEARER);
+	http_route_set_decode_data (jeeves_auth_route, jeeves_user_parse_from_json, jeeves_user_delete);
 	http_route_child_add (jeeves_route, jeeves_auth_route);
+
+}
+
+static void jeeves_set_users_routes (HttpCerver *http_cerver) {
+
+	/* register top level route */
+	// GET /api/users
+	HttpRoute *users_route = http_route_create (REQUEST_METHOD_GET, "api/users", users_handler);
+	http_cerver_route_register (http_cerver, users_route);
+
+	/* register users children routes */
+	// POST api/users/login
+	HttpRoute *users_login_route = http_route_create (REQUEST_METHOD_POST, "login", users_login_handler);
+	http_route_child_add (users_route, users_login_route);
+
+	// POST api/users/register
+	HttpRoute *users_register_route = http_route_create (REQUEST_METHOD_POST, "register", users_register_handler);
+	http_route_child_add (users_route, users_register_route);
 
 }
 
@@ -78,11 +101,15 @@ static void start (void) {
 
 		http_cerver_set_uploads_path (http_cerver, JEEVES_UPLOADS_TEMP_DIR);
 
-		// http_cerver_auth_set_jwt_algorithm (http_cerver, JWT_ALG_RS256);
-		// http_cerver_auth_set_jwt_priv_key_filename (http_cerver, "keys/key.key");
-		// http_cerver_auth_set_jwt_pub_key_filename (http_cerver, "keys/key.pub");
+		http_cerver_auth_set_jwt_algorithm (http_cerver, JWT_ALG_RS256);
+		http_cerver_auth_set_jwt_priv_key_filename (http_cerver, "keys/key.key");
+		http_cerver_auth_set_jwt_pub_key_filename (http_cerver, "keys/key.pub");
 
 		jeeves_set_routes (http_cerver);
+
+		if (ENABLE_USERS_ROUTES) {
+			jeeves_set_users_routes (http_cerver);
+		}
 
 		// add a catch all route
 		http_cerver_set_catch_all_route (http_cerver, jeeves_catch_all_handler);
