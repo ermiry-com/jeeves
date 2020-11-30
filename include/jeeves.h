a121b57b66bc6270ffac20ba8a9e9a3e60ff4408
ermiry <erick_salas@ermiry.com>
2020-11-30 02:35:56 -0600

    Getting more configuration values from environment

--- include/jeeves.h
+++ include/jeeves.h
@@ -1,16 +1,26 @@
 #ifndef _JEEVES_H_
 #define _JEEVES_H_
 
+#include <stdbool.h>
+
 #define JEEVES_UPLOADS_TEMP_DIR                   "/var/uploads"
 
 struct _HttpReceive;
 struct _HttpRequest;
+struct _HttpResponse;
 
 extern unsigned int PORT;
 
 extern unsigned int CERVER_RECEIVE_BUFFER_SIZE;
 extern unsigned int CERVER_TH_THREADS;
 extern unsigned int CERVER_CONNECTION_QUEUE;
+
+extern bool ENABLE_USERS_ROUTES;
+
+extern struct _HttpResponse *oki_doki;
+extern struct _HttpResponse *bad_request;
+extern struct _HttpResponse *server_error;
+extern struct _HttpResponse *bad_user;
 
 #pragma region main
 
--- src/jeeves.c
+++ src/jeeves.c
@@ -15,9 +15,17 @@
 #include <cerver/utils/utils.h>
 #include <cerver/utils/log.h>
 
-#include "jeeves.h"
 #include "handler.h"
+#include "jeeves.h"
+#include "mongo.h"
 #include "version.h"
+
+#include "models/action.h"
+#include "models/role.h"
+#include "models/user.h"
+
+#include "controllers/roles.h"
+#include "controllers/users.h"
 
 unsigned int PORT = CERVER_DEFAULT_PORT;
 
@@ -25,10 +33,16 @@ unsigned int CERVER_RECEIVE_BUFFER_SIZE = CERVER_DEFAULT_RECEIVE_BUFFER_SIZE;
 unsigned int CERVER_TH_THREADS = CERVER_DEFAULT_POOL_THREADS;
 unsigned int CERVER_CONNECTION_QUEUE = CERVER_DEFAULT_CONNECTION_QUEUE;
 
-static HttpResponse *oki_doki = NULL;
-static HttpResponse *bad_request = NULL;
-static HttpResponse *server_error = NULL;
-static HttpResponse *bad_user = NULL;
+static const String *MONGO_URI = NULL;
+static const String *MONGO_APP_NAME = NULL;
+static const String *MONGO_DB = NULL;
+
+bool ENABLE_USERS_ROUTES = false;
+
+HttpResponse *oki_doki = NULL;
+HttpResponse *bad_request = NULL;
+HttpResponse *server_error = NULL;
+HttpResponse *bad_user = NULL;
 
 static HttpResponse *jeeves_works = NULL;
 static HttpResponse *current_version = NULL;
@@ -107,6 +121,86 @@ static void jeeves_env_get_connection_queue (void) {
 
 }
 
+static unsigned int jeeves_env_get_mongo_app_name (void) {
+
+	unsigned int retval = 1;
+
+	char *mongo_app_name_env = getenv ("MONGO_APP_NAME");
+	if (mongo_app_name_env) {
+		MONGO_APP_NAME = str_new (mongo_app_name_env);
+
+		retval = 0;
+	}
+
+	else {
+		cerver_log_error ("Failed to get MONGO_APP_NAME from env!");
+	}
+
+	return retval;
+
+}
+
+static unsigned int jeeves_env_get_mongo_db (void) {
+
+	unsigned int retval = 1;
+
+	char *mongo_db_env = getenv ("MONGO_DB");
+	if (mongo_db_env) {
+		MONGO_DB = str_new (mongo_db_env);
+
+		retval = 0;
+	}
+
+	else {
+		cerver_log_error ("Failed to get MONGO_DB from env!");
+	}
+
+	return retval;
+
+}
+
+static unsigned int jeeves_env_get_mongo_uri (void) {
+
+	unsigned int retval = 1;
+
+	char *mongo_uri_env = getenv ("MONGO_URI");
+	if (mongo_uri_env) {
+		MONGO_URI = str_new (mongo_uri_env);
+
+		retval = 0;
+	}
+
+	else {
+		cerver_log_error ("Failed to get MONGO_URI from env!");
+	}
+
+	return retval;
+
+}
+
+static void jeeves_env_get_enable_users_routes (void) {
+
+	char *enable_users = getenv ("ENABLE_USERS_ROUTES");
+	if (enable_users) {
+		if (!strcmp (enable_users, "TRUE")) {
+			ENABLE_USERS_ROUTES = true;
+			cerver_log_success ("ENABLE_USERS_ROUTES -> TRUE\n");
+		}
+
+		else {
+			ENABLE_USERS_ROUTES = false;
+			cerver_log_success ("ENABLE_USERS_ROUTES -> FALSE\n");
+		}
+	}
+
+	else {
+		cerver_log_warning (
+			"Failed to get ENABLE_USERS_ROUTES from env - using default FALSE!"
+		);
+	}
+
+}
+
 static unsigned int jeeves_init_env (void) {
 
 	unsigned int errors = 0;
@@ -118,6 +212,14 @@ static unsigned int jeeves_init_env (void) {
 	jeeves_env_get_th_threads ();
 
 	jeeves_env_get_connection_queue ();
+
+	errors |= jeeves_env_get_mongo_uri ();
+
+	errors |= jeeves_env_get_mongo_app_name ();
+
+	errors |= jeeves_env_get_mongo_db ();
+
+	jeeves_env_get_enable_users_routes ();
 
 	return errors;
 
@@ -168,6 +270,61 @@ static unsigned int jeeves_init_responses (void) {
 
 }
 
+static unsigned int jeeves_mongo_connect (void) {
+
+	unsigned int errors = 0;
+
+	bool connected_to_mongo = false;
+
+	mongo_set_uri (MONGO_URI->str);
+	mongo_set_app_name (MONGO_APP_NAME->str);
+	mongo_set_db_name (MONGO_DB->str);
+
+	if (!mongo_connect ()) {
+		// test mongo connection
+		if (!mongo_ping_db ()) {
+			cerver_log_success ("Connected to Mongo DB!");
+
+			// open handle to actions collection
+			errors |= actions_collection_get ();
+
+			// open handle to roles collection
+			errors |= roles_collection_get ();
+
+			// open handle to users collection
+			errors |= users_collection_get ();
+
+			connected_to_mongo = true;
+		}
+	}
+
+	if (!connected_to_mongo) {
+		cerver_log_error ("Failed to connect to mongo!");
+		errors |= 1;
+	}
+
+	return errors;
+
+}
+
+static unsigned int jeeves_mongo_init (void) {
+
+	unsigned int retval = 1;
+
+	if (!jeeves_mongo_connect ()) {
+		if (!jeeves_roles_init ()) {
+			retval = 0;
+		}
+
+		else {
+			cerver_log_error ("Failed to get roles from db!");
+		}
+	}
+
+	return retval;
+
+}
+
 // inits jeeves main values
 unsigned int jeeves_init (void) {
 
@@ -176,7 +333,11 @@ unsigned int jeeves_init (void) {
 	if (!jeeves_init_env ()) {
 		unsigned int errors = 0;
 
+		errors |= jeeves_mongo_init ();
+
 		errors |= jeeves_handler_init ();
+
+		errors |= jeeves_users_init ();
 
 		errors |= jeeves_init_responses ();
 
@@ -184,6 +345,22 @@ unsigned int jeeves_init (void) {
 	}
 
 	return retval;
+
+}
+
+static unsigned int jeeves_mongo_end (void) {
+
+	if (mongo_get_status () == MONGO_STATUS_CONNECTED) {
+		actions_collection_close ();
+
+		roles_collection_close ();
+
+		users_collection_close ();
+
+		mongo_disconnect ();
+	}
+
+	return 0;
 
 }
 
@@ -200,7 +377,17 @@ unsigned int jeeves_end (void) {
 	http_respponse_delete (jeeves_works);
 	http_respponse_delete (current_version);
 
+	errors |= jeeves_mongo_end ();
+
+	jeeves_roles_end ();
+
+	jeeves_users_end ();
+
 	jeeves_handler_end ();
+
+	str_delete ((String *) MONGO_URI);
+	str_delete ((String *) MONGO_APP_NAME);
+	str_delete ((String *) MONGO_DB);
 
 	return errors;
 
