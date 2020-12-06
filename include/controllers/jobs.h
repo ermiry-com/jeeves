3667feb02bf27f22c380fedc346e01167134d084
ermiry <erick_salas@ermiry.com>
2020-12-06 10:16:53 -0600

    Added more jobs controller methods

--- include/controllers/jobs.h
+++ include/controllers/jobs.h
@@ -1,10 +1,20 @@
 #ifndef _JEEVES_JOBS_H_
 #define _JEEVES_JOBS_H_
 
+#include <cerver/types/string.h>
+
+#include "models/job.h"
+
 #define DEFAULT_JOBS_POOL_INIT			16
 
 extern unsigned int jeeves_jobs_init (void);
 
 extern void jeeves_jobs_end (void);
+
+extern JeevesJob *jeeves_job_get_by_id_and_user (
+	const String *job_id, const bson_oid_t *user_oid
+);
+
+extern void jeeves_job_return (void *jobs_ptr);
 
 #endif
--- src/controllers/jobs.c
+++ src/controllers/jobs.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <cerver/types/string.h>
+
 #include <cerver/collections/pool.h>
 
 #include <cerver/utils/utils.h>
@@ -14,6 +16,8 @@
 #include "controllers/jobs.h"
 
 static Pool *jobs_pool = NULL;
+
+void jeeves_job_return (void *jobs_ptr);
 
 static unsigned int jeeves_jobs_init_pool (void) {
 
@@ -54,5 +58,38 @@ void jeeves_jobs_end (void) {
 
 	pool_delete (jobs_pool);
 	jobs_pool = NULL;
+
+}
+
+JeevesJob *jeeves_job_get_by_id_and_user (
+	const String *job_id, const bson_oid_t *user_oid
+) {
+
+	JeevesJob *job = NULL;
+
+	if (job_id && user_oid) {
+		job = (JeevesJob *) pool_pop (jobs_pool);
+		if (job) {
+			bson_oid_init_from_string (&job->oid, job_id->str);
+
+			if (jeeves_job_get_by_oid_and_user (
+				job,
+				&job->oid, user_oid,
+				NULL
+			)) {
+				jeeves_job_return (job);
+				job = NULL;
+			}
+		}
+	}
+
+	return job;
+
+}
+
+void jeeves_job_return (void *jobs_ptr) {
+
+	(void) memset (jobs_ptr, 0, sizeof (JeevesJob));
+	(void) pool_push (jobs_pool, jobs_ptr);
 
 }
