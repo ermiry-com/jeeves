36014ce5ecbc9ff795397fd58ec0da137dc4f7db
ermiry <erick.salas@ermiry.com>
2021-03-17 23:14:09 -0600

    Refactored get all jobs handler with new methods
    Also updated jobs handlers with latest responses

--- src/routes/jobs.c
+++ src/routes/jobs.c
@@ -2,57 +2,25 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <cerver/types/types.h>
+#include <cerver/types/string.h>
+
 #include <cerver/http/http.h>
 #include <cerver/http/request.h>
 #include <cerver/http/response.h>
 #include <cerver/http/json/json.h>
 
-#include <cerver/utils/utils.h>
 #include <cerver/utils/log.h>
+#include <cerver/utils/utils.h>
 
 #include "errors.h"
 #include "jeeves.h"
-#include "mongo.h"
 #include "worker.h"
 
 #include "models/job.h"
 #include "models/user.h"
 
 #include "controllers/jobs.h"
-
-static char *jeeves_get_jobs_handler_generate_json (
-	mongoc_cursor_t *jobs_cursor,
-	size_t *json_len
-) {
-
-	char *retval = NULL;
-
-	bson_t *doc = bson_new ();
-	if (doc) {
-		bson_t jobs_array = { 0 };
-		(void) bson_append_array_begin (doc, "jobs", -1, &jobs_array);
-		char buf[16] = { 0 };
-		const char *key = NULL;
-		size_t keylen = 0;
-
-		int i = 0;
-		const bson_t *job_doc = NULL;
-		while (mongoc_cursor_next (jobs_cursor, &job_doc)) {
-			keylen = bson_uint32_to_string (i, &key, buf, sizeof (buf));
-			(void) bson_append_document (&jobs_array, key, (int) keylen, job_doc);
-
-			bson_destroy ((bson_t *) job_doc);
-
-			i++;
-		}
-		(void) bson_append_array_end (doc, &jobs_array);
-
-		retval = bson_as_relaxed_extended_json (doc, json_len);
-	}
-
-	return retval;
-
-}
 
 // GET /api/jeeves/jobs
 // Returns all of the authenticated user's jobs
@@ -63,35 +31,20 @@ void jeeves_get_jobs_handler (
 
 	User *user = (User *) request->decoded_data;
 	if (user) {
-		bson_oid_init_from_string (&user->oid, user->id);
-
-		// get user's jobs from the db
-		mongoc_cursor_t *jobs_cursor = jeeves_jobs_get_all_by_user (
-			&user->oid, NULL
+		size_t json_len = 0;
+		char *json = transactions_get_all_by_user_to_json (
+			&user->oid, NULL,
+			&json_len
 		);
 
-		if (jobs_cursor) {
-			// convert them to json and send them back
-			size_t json_len = 0;
-			char *json = jeeves_get_jobs_handler_generate_json (
-				jobs_cursor, &json_len
+		if (json) {
+			(void) http_response_json_custom_reference_send (
+				http_receive,
+				200,
+				json, json_len
 			);
 
-			if (json) {
-				(void) http_response_json_custom_reference_send (
-					http_receive,
-					200,
-					json, json_len
-				);
-
-				free (json);
-			}
-
-			else {
-				(void) http_response_send (server_error, http_receive);
-			}
-
-			mongoc_cursor_destroy (jobs_cursor);
+			free (json);
 		}
 
 		else {
@@ -100,7 +53,7 @@ void jeeves_get_jobs_handler (
 	}
 
 	else {
-		(void) http_response_send (bad_user, http_receive);
+		(void) http_response_send (bad_user_error, http_receive);
 	}
 
 }
@@ -118,12 +71,16 @@ static void jeeves_job_parse_json (
 		json_object_foreach (json_body, key, value) {
 			if (!strcmp (key, "name")) {
 				*name = json_string_value (value);
+				#ifdef JEEVES_DEBUG
 				(void) printf ("name: \"%s\"\n", *name);
+				#endif
 			}
 
 			else if (!strcmp (key, "description")) {
 				*description = json_string_value (value);
+				#ifdef JEEVES_DEBUG
 				(void) printf ("description: \"%s\"\n", *description);
+				#endif
 			}
 		}
 	}
@@ -228,7 +185,7 @@ void jeeves_create_job_handler (
 	}
 
 	else {
-		(void) http_response_send (bad_user, http_receive);
+		(void) http_response_send (bad_user_error, http_receive);
 	}
 
 }
@@ -292,7 +249,7 @@ void jeeves_job_info_handler (
 	}
 
 	else {
-		(void) http_response_send (bad_user, http_receive);
+		(void) http_response_send (bad_user_error, http_receive);
 	}
 
 }
@@ -427,7 +384,7 @@ void jeeves_job_config_handler (
 	}
 
 	else {
-		(void) http_response_send (bad_user, http_receive);
+		(void) http_response_send (bad_user_error, http_receive);
 	}
 
 }
@@ -551,7 +508,7 @@ void jeeves_job_upload_handler (
 
 	else {
 		http_request_multi_part_discard_files (request);
-		(void) http_response_send (bad_user, http_receive);
+		(void) http_response_send (bad_user_error, http_receive);
 	}
 
 }
@@ -629,7 +586,7 @@ void jeeves_job_start_handler (
 	}
 
 	else {
-		(void) http_response_send (bad_user, http_receive);
+		(void) http_response_send (bad_user_error, http_receive);
 	}
 
 }
@@ -672,7 +629,7 @@ void jeeves_job_stop_handler (
 	}
 
 	else {
-		(void) http_response_send (bad_user, http_receive);
+		(void) http_response_send (bad_user_error, http_receive);
 	}
 
 }
