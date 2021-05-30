32b07826e048ef79f9ca57ee7142593db98f98e9
ermiry <erick.salas@ermiry.com>
2021-05-29 23:47:29 -0500

    Refactored internal version methods

--- src/version.c
+++ src/version.c
@@ -5,22 +5,41 @@
 // print full jeeves version information
 void jeeves_version_print_full (void) {
 
-	cerver_log_msg ("Jeeves Service Version: %s", JEEVES_VERSION_NAME);
-	cerver_log_msg ("Release Date & time: %s - %s", JEEVES_VERSION_DATE, JEEVES_VERSION_TIME);
-	cerver_log_msg ("Author: %s", JEEVES_VERSION_AUTHOR);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Jeeves Service Version: %s", JEEVES_VERSION_NAME
+	);
+
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Release Date & time: %s - %s", JEEVES_VERSION_DATE, JEEVES_VERSION_TIME
+	);
+
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Author: %s\n", JEEVES_VERSION_AUTHOR
+	);
 
 }
 
 // print the version id
 void jeeves_version_print_version_id (void) {
 
-	cerver_log_msg ("Jeeves Service Version ID: %s", JEEVES_VERSION);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Jeeves Service Version ID: %s\n",
+		JEEVES_VERSION
+	);
 
 }
 
 // print the version name
 void jeeves_version_print_version_name (void) {
 
-	cerver_log_msg ("Jeeves Service Version: %s", JEEVES_VERSION_NAME);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Jeeves Service Version: %s\n",
+		JEEVES_VERSION_NAME
+	);
 
 }
