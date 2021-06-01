a664cec2971d74f4c8e3313a0ee679d025ead5eb
ermiry <erick.salas@ermiry.com>
2021-05-31 19:19:26 -0500

    Updated version to 0.3

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 #ifndef _JEEVES_VERSION_H_
 #define _JEEVES_VERSION_H_
 
-#define JEEVES_VERSION                    	"0.2"
-#define JEEVES_VERSION_NAME               	"Version 0.2"
-#define JEEVES_VERSION_DATE			    	"08/12/2020"
-#define JEEVES_VERSION_TIME			    	"18:36 CST"
-#define JEEVES_VERSION_AUTHOR			    "Erick Salas"
+#define JEEVES_VERSION				"0.3"
+#define JEEVES_VERSION_NAME			"Version 0.3"
+#define JEEVES_VERSION_DATE			"31/05/2021"
+#define JEEVES_VERSION_TIME			"19:19 CST"
+#define JEEVES_VERSION_AUTHOR		"Erick Salas"
 
 // print full jeeves version information
 extern void jeeves_version_print_full (void);
